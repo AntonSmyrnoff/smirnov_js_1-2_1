8b1937c2cd5920790838d59dd2c36fb8ed580329
Anton Smyrnov <a.smyrnoff@gmail.com>
2016-11-01 23:53:48 +0200

    checkings

--- js/script.js
+++ js/script.js
@@ -1,29 +1,38 @@
-var base = +prompt("Enter the Base number");
-var exp = +prompt("Enter the Exponent");
-
-
-if (isNaN(base) || isNaN(exp)) 
-  {console.log ('One or both argument is not number')} 
-else 
-  {console.log ('Result: ', base, '^', exp, '=', pow())}
-
+var base = prompt("Enter the Base number");
+var exp = prompt("Enter the Exponent");
+ if (base == '' || base == null || exp == '' || exp == null) 
+   {console.log ('Error. Reload page and enter integer.')}
+ else
+   { 
+    base = +base;
+    exp = +exp;
+    console.log (base, exp);
+    pow();
+   } 
 
 function pow() {
+  if ( isNaN(base) || isNaN(exp) || parseInt (base) !== base || parseInt (exp) !== exp ) 
+    {console.log ('Error. Reload page and enter integer.')} 
+  else 
+    {console.log ('Result: ', base, '^', exp, '=', check())}
+}
+
+function check() {
   var result = 1;  
   if (exp >= 0) 
     {
-      result = raise (base, exp, result);
+      result = raise(base, exp, result);
     }
   else 
     {
       exp = -exp;
-      result = 1 / raise (base, exp, result);
+      result = 1 / raise(base, exp, result);
     }
 
   return result;
 }
 
-function raise (base, exp, result) 
+function raise(base, exp, result) 
 {
   for (var i = 0; i < exp; i++) 
   {
